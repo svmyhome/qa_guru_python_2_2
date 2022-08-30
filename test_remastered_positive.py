1a8d846cf4e9b5fb6ccf39d2fef2e61729707fbc
Vladimir Sarychev <svmyhome@mail.ru>
2022-08-30 23:23:05 +0300

    Дополнил дополнил позитивными и негативными провеками

--- test_remastered_positive.py
+++ test_remastered_positive.py
@@ -12,6 +12,8 @@ gender = "Male"
 student_name = "Ivan Ivanov"
 mobile = "1234567890"
 
+selene_name = "yashaka/selene: User-oriented Web UI browser tests in Python"
+
 
 @pytest.fixture()
 def open_browser():
@@ -21,23 +23,22 @@ def open_browser():
 def configure_mobile_browser(open_browser):
     browser.config.window_width = 375
     browser.config.window_height = 667
-    browser.open('https://demoqa.com/automation-practice-form')
 
 @pytest.fixture()
 def configure_desktop_browser(open_browser):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+
+
+def test_first_page_mobile_positive(configure_mobile_browser):
     browser.open('https://demoqa.com/automation-practice-form')
-
-
-def test_first_page_mobile(configure_mobile_browser):
     assert name_form == browser.element("//div[text()='Practice Form']").text
     browser.element("//input[@id = 'firstName']").type(first_name)
     browser.element("//input[@id = 'lastName']").type(last_name)
     browser.element("//input[@id = 'userEmail']").type(user_email)
     browser.element("//label[text() = '" + gender + "']").click()
     browser.element("//input[@id = 'userNumber']").type(mobile).press_enter()
-    time.sleep(1)  # разве селен не автоматический выставляет задержку
+    time.sleep(1)  #TODO разве селен не автоматический выставляет задержку
     assert fill_in_form_title == browser.element("//div[@id='example-modal-sizes-title-lg']").text
     assert student_name == browser.element("//td[text()='Student Name']/following-sibling::td").text
     assert user_email == browser.element("//td[text()='Student Email']/following-sibling::td").text
@@ -45,17 +46,38 @@ def test_first_page_mobile(configure_mobile_browser):
     assert mobile == browser.element("//td[text()='Mobile']/following-sibling::td").text
     time.sleep(3)
 
-def test_first_page_desktop(configure_desktop_browser):
+def test_first_page_desktop_positive(configure_desktop_browser):
+    browser.open('https://demoqa.com/automation-practice-form')
     assert name_form == browser.element("//div[text()='Practice Form']").text
     browser.element("//input[@id = 'firstName']").type(first_name)
     browser.element("//input[@id = 'lastName']").type(last_name)
     browser.element("//input[@id = 'userEmail']").type(user_email)
     browser.element("//label[text() = '" + gender + "']").click()
     browser.element("//input[@id = 'userNumber']").type(mobile).press_enter()
-    time.sleep(1) # разве селен не автоматический выставляет задержку
+    time.sleep(1) #TODO разве селен не автоматический выставляет задержку
     assert fill_in_form_title == browser.element("//div[@id='example-modal-sizes-title-lg']").text
     assert student_name == browser.element("//td[text()='Student Name']/following-sibling::td").text
     assert user_email == browser.element("//td[text()='Student Email']/following-sibling::td").text
     assert gender == browser.element("//td[text()='Gender']/following-sibling::td").text
     assert mobile == browser.element("//td[text()='Mobile']/following-sibling::td").text
     time.sleep(3)
+
+
+def test_second_page_mobile_positive(configure_mobile_browser):
+    browser.open('https://google.com')
+    browser.element('//input[@title="Поиск"]').should(be.blank).type('selene').press_enter()
+    browser.element('//div[@id="search"]').should(have.text(selene_name))
+def test_second_page_desktop_positive(configure_desktop_browser):
+    browser.open('https://google.com')
+    browser.element('//input[@title="Поиск"]').should(be.blank).type('selene').press_enter()
+    browser.element('//div[@id="search"]').should(have.text(selene_name))
+
+def test_first_page_desktop_negative(configure_desktop_browser):
+    browser.open('https://google.com')
+    browser.element('//input[@title="Поиск"]').should(be.blank).type('selene1111').press_enter()
+    browser.element('//div[@id="search"]').should(have.no.text(selene_name))
+
+def test_second_page_desktop_negative(configure_desktop_browser):
+    browser.open('https://google.com')
+    browser.element('//input[@title="Поиск"]').should(be.blank).type('selene1111').press_enter()
+    assert selene_name != browser.element('//div[@id="search"]').text
