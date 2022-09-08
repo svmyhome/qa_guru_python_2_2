90338a76ae9c45839f8c60cdcff23bdd10603710
Vladimir Sarychev <vladimir.sarychev@lenta.com>
2022-09-08 18:22:57 +0300

    Цикл в методе print_name_function заменен на replace

--- homeworks/print_fanction_name.py
+++ homeworks/print_fanction_name.py
@@ -2,27 +2,21 @@
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
 def print_name_function(function_name, *args):
-    name_func = function_name.__name__.title().split("_")
-    for i in range((len(name_func))):
-        print(name_func[i], end=" ")
-    print();
+    name_func = function_name.__name__.title().replace("_", " ")
+    print("=" * 10, "Start", "=" * 10)
+    print(name_func)
     for arg in args:
         print(arg.title())
+    print("=" * 10, "End", "=" * 10)
 
 def open_browser(browser_name):
-    print("=" * 10, "Start", "=" * 10)
     print_name_function(open_browser, browser_name)
-    print("=" * 10, "End", "=" * 10)
 
 def go_to_companyname_homepage(page_url):
-    print("=" * 10, "Start", "=" * 10)
     print_name_function(go_to_companyname_homepage, page_url)
-    print("=" * 10, "End", "=" * 10)
 
 def find_registration_button_on_login_page(page_url, button_text):
-    print("=" * 10, "Start", "=" * 10)
     print_name_function(find_registration_button_on_login_page, page_url, button_text)
-    print("=" * 10, "End", "=" * 10)
 
 open_browser("chrome")
 go_to_companyname_homepage("url")
